9c89f113787ccc5bbc2360fa3a6aa05534873f47
Balint Apro <balint.apro@falcon.io>
2024-04-19 17:52:07 +0200

    feat: migrate to vite

--- react-poke/src/components/Header.jsx
+++ react-poke/src/components/Header.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <a href="http://localhost:4200/" alt="switch to Angular">
-        <img alt="react" src={process.env.PUBLIC_URL + '/logo192.png'} />
+        <img alt="react" src={'/logo192.png'} />
       </a>
       <NavLink to="/">
         <h1>Pokedex</h1>
       </NavLink>
     </header>
   );
-}
+};
 
 export default Header;
